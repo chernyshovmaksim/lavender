5dddef1d36bcf18ad5312edd1d601ff2142f4109
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2023-09-25 12:55:29 +0500

    Changes to be committed: 	new file:   public/assets/img/hero-one.webp 	deleted:    resources/views/auth/confirm-password.blade.php 	deleted:    resources/views/auth/forgot-password.blade.php 	deleted:    resources/views/auth/login.blade.php 	deleted:    resources/views/auth/register.blade.php 	deleted:    resources/views/auth/reset-password.blade.php 	deleted:    resources/views/auth/verify-email.blade.php 	modified:   resources/views/components/parts/hero.blade.php

--- resources/views/components/parts/hero.blade.php
+++ resources/views/components/parts/hero.blade.php
@@ -1,6 +1,8 @@
 <div>
     <div class="container p-4 mt-16 bg-white rounded shadow grid grid-cols-4 grid-rows-2 gap-4">
-        <div class="row-start-1 row-end-3 col-start-1 col-end-3 bg-slate-200 rounded">one</div>
+        <div class="row-start-1 row-end-3 col-start-1 col-end-3 bg-slate-200 rounded relative overflow-hidden"> 
+                <img src="/assets/img/hero-one.webp" class="w-full h-full object-cover" alt="">
+        </div>
         <div class="row-start-1 row-end-2 col-start-3 col-end-4 bg-slate-200 rounded">two</div>
         <div class="row-start-1 row-end-2 col-start-4 col-end-5 bg-lavender rounded min-h-[200px] flex items-center justify-center flex-col">
             <a href="/" class="text-white text-center font-bold">Заказать звонок <br />от флориста</a>
